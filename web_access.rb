071a114127dea3babcb36515c4de943ea615afdd
adi <aofry@yahoo.com>
2013-07-03 23:32:06 +0300

    remove the class

--- web_access.rb
+++ web_access.rb
@@ -1,17 +1,11 @@
 require 'sinatra'
 require './iaas_controller.rb'
 
-class WebAccess
 
-  def initialize(name)
+iaaSController = IaaSController.new("World")
 
-    get '/farmInfo' do
-      puts "before getFarmInfo"
-      @iaaSController = IaaSController.new("World")
-      @iaaSController.getFarmInfo
-    end
-  end
+get '/farmInfo' do
+  puts "before getFarmInfo"
+  iaaSController.getFarmInfo
 end
-
-WebAccess.new("world2")
 
